784cdc5d4bcf1ba794f55a81cf926ac7de7dfa69
Ricky <ballard.programmer@gmail.com>
2019-10-01 22:38:06 +0100

    Update index.js
    Updated to work on a server better, removed pointless console logs, removed pointless comments
    structured it better so that functions are not "Chained" - allowing better future functionality

--- index.js
+++ index.js
@@ -6,129 +6,116 @@ const settings = require("./assets/settings.json");
 // Require the file system to be able to save the information
 const fs = require("fs");
 
-async function marketPage(browser, page){
-    // Scrape Market data
-    await page.content();
-    await page.goto(
-        // Disable timout error due to time it takes to load page!
-        `${settings.URL.SalesPage}`,{
-            timeout: 0,
-        }
-        );
-    console.log("Loaded Sales API, Apologies for the delay!");
-    const salesApi = await page.content();
-    const $ = cheerio.load(salesApi);
-    const salesStr = $('body > pre').text();
-    fs.writeFileSync("../bots/GuildLife/scraped_files/current_sales.json", salesStr);
-    console.log("Sales list extracted!");
-    const closeProcess = browser.close();
-    await closeProcess;
-}
-
-async function charPage(browser, page){
-    // Nothing to use here just yet - navigate to Trade page
-    console.log("Char page is more for personal use - Navigated here in-case of future needs!");
-    console.log("Will need to add a loop functionality to be able to select the right trade post if you have a favourite!");
-    await page.evaluate(() => {
-        // Most likely multiple items here - default to first
-        // At some point, will need to iterate through to select right trade post!
-        document.querySelector('.char_guild_row .char_guild_col1 a').click();
-    });
-    await page.waitForNavigation();
-    console.log("Changed to market page, this will take a minute or two!");
-    const markPage = marketPage(browser, page);
-    await markPage;
-}
-
-async function scrapePlayerCount(browser, page){
-    const charName = settings.character;
-    // Write the player count data to file!
-    const data = await page.content();
-    const $ = cheerio.load(data);
-    // Get the information
-    const plCount = $('#wrapper > div.container.clearfix > div.news_main > div > div > div.dashboard > div.dashboard_row2 > div.dashboard_world > div:nth-child(6)').text();
-    // Retrieve the "title" from the string
-    const plCountTitle = plCount.substring(0,14);
-    // retrieve the "amount" from the string
-    const plCountAmount = plCount.substring(15, 18);
-    // convert amount to a number
-    const plCountNo = parseInt(plCountAmount);
-    // Place into an Object
-    const plCountObj = { 
-        Title: plCountTitle, 
-        Amount: plCountNo 
-    };
-    // Convert Object to string and output in a readable format, keeping it parsed to use
-    const plCountOut = JSON.stringify(plCountObj, null, "\t");
-    fs.writeFileSync("../bots/GuildLife/scraped_files/player_count.json", plCountOut);
-    console.log("Got Player count!");
-    // Once data is scraped, Navigate to next page! 
-    await page.evaluate(() => {
-        // If multiple elements here - defaults to first one (First character in list)
-        document.querySelector('.world_char .name a').click();
-    });
-    await page.waitForNavigation();
-    console.log("Changed page to profile page.");
-    const charsPage = charPage(browser, page);
-    await charsPage;
-}
-
-async function accPage(browser, page){
-    console.log("Loading...");
+async function marketPage(page){
     try {
-    await page.waitFor('.bil_submenu_content');
-    await page.evaluate(() => {
-        document.querySelector('.bil_submenu_content a').click();
-    });
-    await page.waitForSelector('.world_char');
-    console.log("Changed page to: " + page.url());
-    const playerCount = scrapePlayerCount(browser, page);
-    await playerCount;
-    } catch (error){
-        console.error(error);
-    }
-}
-
-async function signIn(browser, page, $){
-    try {
-    // Define log-in Details
-    const password = settings.password;
-    const email = settings.email;
-    // Go through the log-in process here
-        const clickSignIn = async (page, text) => {
-            const loginPage = await page.content();
-            // Have to "wait" for elements before inputting.
-            await page.waitFor('input[name="email"]');
-            // Input data.
-            await page.type('input[name="email"]', `${email}`);
-            await page.waitFor('input[name = "password"]');
-            await page.type('input[name="password"]', `${password}`);
-            await page.evaluate(() => {
-                document.querySelector('.button-with-bg').click();
-            });
-            await page.waitForNavigation();
-            // Confirm log-in
-            console.log("Logged in at: " + page.url());
-            const accountPage = accPage(browser, page);
-            await accountPage;
-        }
-    // Action above functions
-    await clickSignIn(page, 'Sign In');
+        await page.goto(
+            // Disable timout error due to time it takes to load page!
+            `${settings.URL.SalesPage}`,{
+                timeout: 0,
+            }
+            );
+        console.log("Loaded Sales API, apologies for the delay!");
+        const salesApi = await page.content();
+        const $ = cheerio.load(salesApi);
+        const salesStr = $('body > pre').text();
+        fs.writeFileSync("../bots/GuildLife/scraped_files/current_sales.json", salesStr);
+        console.log("Sales list extracted!");
     } catch (error) {
         console.error(error);
     }
 }
 
-async function loginPage(browser, page){
+async function charPage(page){
     try {
-    // Navigate to login page
-    await page.goto(`${settings.URL.Login}`);
-    // Get page Data
-    const content = await page.content();
-    // load the page to grab elements
-    const $ = cheerio.load(content);
-    const logIn = await signIn(browser, page, $);
-    logIn;
+        // Nothing to use here just yet maybe in future - navigate to Trade page
+        await page.evaluate(() => {
+            // Most likely multiple items here - default to first - Will need to create an iteration for trade posts!
+            document.querySelector('.char_guild_row .char_guild_col1 a').click();
+        });
+        await page.waitForNavigation();
+        console.log("Redirected to market, this will take a minute or two!");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function scrapePlayerCount(page){
+    try {
+        const data = await page.content();
+        const $ = cheerio.load(data);
+        // Get the information
+        const plCount = $('#wrapper > div.container.clearfix > div.news_main > div > div > div.dashboard > div.dashboard_row2 > div.dashboard_world > div:nth-child(6)').text();
+        // Title
+        const plCountTitle = plCount.substring(0,14);
+        // Amount
+        const plCountAmount = plCount.substring(15, 19);
+        // Place into an Object
+        const plCountObj = { 
+            Title: plCountTitle, 
+            Amount: plCountAmount 
+        };
+        // Convert Object to string
+        const plCountOut = JSON.stringify(plCountObj, null, "\t");
+        fs.writeFileSync("../bots/GuildLife/scraped_files/player_count.json", plCountOut);
+        console.log("Got Player count!");
+        // Once data is scraped, Navigate to next page! 
+        await page.evaluate(() => {
+            // If multiple elements here - defaults to first selector
+            document.querySelector('.world_char .name a').click();
+        });
+        await page.waitForNavigation();
+        console.log("Redirected to profile page.");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function accPage(page){
+        console.log("Loading...");
+        try {
+        await page.waitFor('.bil_submenu_content');
+        await page.evaluate(() => {
+            document.querySelector('.bil_submenu_content a').click();
+        });
+        await page.waitForSelector('.world_char');
+        console.log("Redirected to Dashboard");
+    } catch (error){
+        console.error(error);
+    }
+}
+
+async function signIn(page){
+    try {
+        // Define log-in Details
+        const password = settings.password;
+        const email = settings.email;
+        // Go through the log-in process here
+            const clickSignIn = async (page, text) => {
+            // const loginPage = await page.content();
+                // Have to "wait" for elements before inputting.
+                await page.waitFor('input[name="email"]');
+                // Input data.
+                await page.type('input[name="email"]', `${email}`);
+                await page.waitFor('input[name = "password"]');
+                await page.type('input[name="password"]', `${password}`);
+                await page.evaluate(() => {
+                    document.querySelector('.button-with-bg').click();
+                });
+                await page.waitForNavigation();
+                // Confirm log-in
+                console.log("Logged in at: " + page.url());
+            }
+        // Action above functions
+        await clickSignIn(page, 'Sign In');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function loginPage(page){
+    try {
+        // Navigate to login page
+        await page.goto(`${settings.URL.Login}`);
     } catch (error) {
         console.error(error);
     }
@@ -136,12 +123,29 @@ async function loginPage(browser, page){
 
 async function initBrowser() {
     try {
-    // Initiate and launch browser - Set headless to true for a server!
-    const browser = await puppeteer.launch({headless: true, defaultViewport: null, args: ['--no-sandbox']});
-    // Open new tab
-    const page = await browser.newPage();
-    // init scraping
-    const content = await loginPage(browser, page);
+        // Initiate and launch browser - Set headless to true for a server!
+        const browser = await puppeteer.launch({headless: true, defaultViewport: null, args: ['--no-sandbox']});
+        // Open new tab
+        const page = await browser.newPage();
+        // init scraping
+        const logInPage = loginPage(page);
+        await logInPage;
+        // Get page Data
+        const content = await page.content();
+        // load the page to grab elements
+        const $ = cheerio.load(content);
+        const logIn = signIn(page, $);
+        await logIn;
+        const accountPage = accPage(page);
+        await accountPage;
+        const playerCount = scrapePlayerCount(page);
+        await playerCount;
+        const charsPage = charPage(page);
+        await charsPage;
+        const markPage = marketPage(page);
+        await markPage;
+        const closeProcess = browser.close();
+        await closeProcess;
     } catch (error) {
         console.error(error);
     }
